5a5efe470c2460c389b23032bb88d7f0d495a880
Mohammad ghazal <moh.g.ghazal@gmail.com>
2021-12-28 21:53:56 +0200

    factorial function

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+function factorial ($nonNegativeInteger)
+{
+    if($nonNegativeInteger === 1)
+    return 1;
+    else
+    return $nonNegativeInteger*factorial($nonNegativeInteger-1);
+}
 
-
+echo factorial(4);
 ?>
