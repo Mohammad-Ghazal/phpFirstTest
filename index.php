e4db035cfbe9d8c28bb45d8ba45e8430a11a6119
Mohammad ghazal <moh.g.ghazal@gmail.com>
2021-12-28 22:00:28 +0200

    reverse function

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
-function factorial ($nonNegativeInteger)
+function factorial($nonNegativeInteger)
 {
-    if($nonNegativeInteger === 1)
-    return 1;
+    if ($nonNegativeInteger === 1)
+        return 1;
     else
-    return $nonNegativeInteger*factorial($nonNegativeInteger-1);
+        return $nonNegativeInteger * factorial($nonNegativeInteger - 1);
 }
 
-echo factorial(4);
-?>
+print_r(factorial(4));
+
+
+
+
+function reverse($string)
+{
+    return strrev($string);
+}
+echo "<br>";
+print_r(strrev('hello'));
+
+
+
